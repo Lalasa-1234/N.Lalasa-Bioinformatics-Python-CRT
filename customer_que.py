5d4d87c3c637731bd87a7be8a2744dc925139447
Namepalli Lalasa <lalasanamepalli@gmail.com>
2025-06-23 20:27:11 +0530

    Delete customer_que.py

--- customer_que.py
+++
@@ -1,8 +0,0 @@
-'''to create the customer servive add customer into que once the customer is served he has to tbe removed from rthe
- queue'''
-'''write a python program to take the length of queue as a input from the user and add the elements by using enque
- and print the elements present in the queu and remove the elements one by one from the que and check wethr the que 
- is empty or not and print front peek and rear peak .'''
-'''Write a Python Program to take the length of q as a input from the user and add the eleements by using enqueue method and 
-print the Elements present in the queue and remove the elements one by one from the queue and check whether the 
-queu is empty or not and print the front peek and rear peek.'''
